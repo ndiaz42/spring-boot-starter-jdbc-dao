62543d7b7eacc621288110ec658629018865b9a4
Nicolas Diaz <nicolasdiaz@protonmail.com>
2020-07-18 20:30:38 -0300

    Add classes to perform pagination
    (the query at least works in DB2)

---
+++ jdbc-dao-core/src/main/java/com/gihub/ndiaz/jdbc/util/CombinedSqlParameterSource.java
@@ -0,0 +1,60 @@
+package com.gihub.ndiaz.jdbc.util;
+
+import java.util.Map;
+import org.springframework.jdbc.core.namedparam.AbstractSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
+public class CombinedSqlParameterSource extends AbstractSqlParameterSource {
+
+  private final MapSqlParameterSource mapSqlParameterSource;
+  private final AbstractSqlParameterSource abstractSqlParameterSource;
+
+  public CombinedSqlParameterSource(final Object object) {
+    mapSqlParameterSource = new MapSqlParameterSource();
+    abstractSqlParameterSource = new BeanPropertySqlParameterSource(object);
+  }
+
+  public CombinedSqlParameterSource(final AbstractSqlParameterSource abstractSqlParameterSource) {
+    mapSqlParameterSource = new MapSqlParameterSource();
+    this.abstractSqlParameterSource = abstractSqlParameterSource;
+  }
+
+  public CombinedSqlParameterSource addValue(final String paramName, final Object value) {
+    mapSqlParameterSource.addValue(paramName, value);
+    return this;
+  }
+
+  public CombinedSqlParameterSource addValue(final String paramName, final Object value,
+                                             final int sqlType) {
+    mapSqlParameterSource.addValue(paramName, value, sqlType);
+    return this;
+  }
+
+  public CombinedSqlParameterSource addValues(final Map<String, ?> values) {
+    mapSqlParameterSource.addValues(values);
+    return this;
+  }
+
+  @Override
+  public boolean hasValue(final String paramName) {
+    return abstractSqlParameterSource.hasValue(paramName)
+        || mapSqlParameterSource.hasValue(paramName);
+  }
+
+  @Override
+  public Object getValue(final String paramName) {
+    return abstractSqlParameterSource.hasValue(paramName)
+        ? abstractSqlParameterSource.getValue(paramName)
+        : mapSqlParameterSource.getValue(paramName);
+  }
+
+  @Override
+  public int getSqlType(final String paramName) {
+    return abstractSqlParameterSource.hasValue(paramName)
+        ? abstractSqlParameterSource.getSqlType(paramName)
+        : mapSqlParameterSource.getSqlType(paramName);
+  }
+
+}
+
