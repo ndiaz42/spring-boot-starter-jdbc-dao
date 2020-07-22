debf66e3b9f9d7617d866ac5d2676994bd4ac501
Nicolas Diaz <nicolasdiaz@protonmail.com>
2020-07-22 12:55:02 -0300

    SqlFileLoader configuration

---
+++ jdbc-dao-spring-boot-autoconfigure/src/main/java/com/github/ndiaz/jdbc/autoconfigure/JdbcDaoProperties.java
@@ -0,0 +1,28 @@
+package com.github.ndiaz.jdbc.autoconfigure;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = JdbcDaoProperties.JDBC_DAO_PREFIX)
+public class JdbcDaoProperties {
+
+  public static final String JDBC_DAO_PREFIX = "dao";
+
+  private Boolean enableSqlFile = false;
+
+  @NestedConfigurationProperty
+  private SqlFileProperties file = new SqlFileProperties();
+
+  @Getter
+  @Setter
+  public static class SqlFileProperties {
+
+    private String path = "db/";
+
+  }
+
+}
