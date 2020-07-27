30d9d412fdeee7db5027cd23b7149a31529b84ef
ndiaz42 <68431578+ndiaz42@users.noreply.github.com>
2020-07-27 09:30:05 -0300

    RowUnmapper JavaDoc

--- jdbc-dao-core/src/main/java/com/github/ndiaz/jdbc/mapper/RowUnmapper.java
+++ jdbc-dao-core/src/main/java/com/github/ndiaz/jdbc/mapper/RowUnmapper.java
@@ -4,10 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.jdbc.core.namedparam.AbstractSqlParameterSource;
 
+/**
+ * A {@code RowUnmapper} transforms a domain object into a map, used by the library when executing
+ * create or update queries.
+ *
+ * @param <T> the domain object's class
+ * @see AbstractSqlParameterSource
+ */
 public interface RowUnmapper<T> {
 
+  /**
+   * Gets the sql parameters as a map.
+   *
+   * @param entity the domain object to map
+   * @return the sql parameters
+   */
   AbstractSqlParameterSource getSqlParameters(final T entity);
 
+  /**
+   * Gets the sql parameters as a list of maps. Used on batch updates.
+   *
+   * @param entities the domain objects to map
+   * @return the list of sql parameters
+   */
   default List<AbstractSqlParameterSource> getSqlParameters(final List<T> entities) {
     final List<AbstractSqlParameterSource> params = new ArrayList<>();
     for (final T entity : entities) {
